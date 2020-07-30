4cb5d3819e4dc79a64475ae8f8193dafa797348c
Bertil Tandayamo <bertilt4@gmail.com>
2020-07-30 15:24:45 -0500

    Project page - Rename to just Project

--- src/container/App.js
+++ src/container/App.js
@@ -1,11 +1,11 @@
 import 'css-reset-and-normalize';
 import '../styles/style.css';
-import Projects from '../pages/Projects';
+import ProjectPage from '../pages/Project';
 // import Tasks from '../pages/Task';
 
 const App = () => {
   const main = document.createElement('main');
-  main.innerHTML = Projects();
+  main.innerHTML = ProjectPage();
   // main.append(Projects());
   // main.append(Tasks);
 
